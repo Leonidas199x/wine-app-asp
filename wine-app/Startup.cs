cdc4776db3c512d8d80f385a37157fd3488d3953
Leonidas199x <toby.w.prince@outlook.com>
2021-03-28 13:05:13 +0100

    Registration of Country service and repo

--- wine-app/Startup.cs
+++ wine-app/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using wine_app.Domain.Country;
 
 namespace wine_app
 {
@@ -24,6 +20,8 @@ namespace wine_app
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<ICountryService, CountryService>();
+            services.AddTransient<ICountryRepository, CountryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
