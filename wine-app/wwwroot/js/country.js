4af0e7aaf9b66801185620226a618215b6170d05
Leonidas199x <toby.w.prince@outlook.com>
2021-03-29 21:14:37 +0100

    Addition of functionality for delete

---
+++ wine-app/wwwroot/js/country.js
@@ -0,0 +1,16 @@
+﻿function deleteCountry(id, name) {
+    if (confirm(`Are you sure you want to delete ${name} from the available countries?`)) {
+        $.ajax({
+            url: `/Country/Delete/${id}`,
+            type: 'DELETE',
+            error: function (err) {
+                console.log(err);
+                alert('Something went wrong...check the console for the error');
+            },
+            success: function () {
+                window.location.href = `/Country/List`;
+            },
+            async: true,
+        });
+    }
+}
