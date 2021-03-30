f9618bed37b77e0564174c32d855396cf6d64438
Leonidas199x <toby.w.prince@gmail.com>
2021-03-30 21:50:33 +0100

    Amendment to error text

--- wine-app/wwwroot/js/country.js
+++ wine-app/wwwroot/js/country.js
@@ -1,11 +1,11 @@
 ﻿function deleteCountry(id, name) {
-    if (confirm(`Are you sure you want to delete ${name} from the available countries?`)) {
+    if (confirm(`Are you sure you want to delete '${name}' from the list of available countries?`)) {
         $.ajax({
             url: `/Country/Delete/${id}`,
             type: 'DELETE',
             error: function (err) {
                 console.log(err);
-                alert('Something went wrong...check the console for the error');
+                alert(`Something went wrong, cannot delete country with name '${name}' and ID ${id}. Error code: ${err.status}`);
             },
             success: function () {
                 window.location.href = `/Country/List`;
