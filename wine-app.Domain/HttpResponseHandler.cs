a6be4ffaa5153557428b010ddda9996edaba88d3
Leonidas199x <toby.w.prince@gmail.com>
2021-03-30 19:18:31 +0100

    Addition of HttpResponseHandler class and result class

---
+++ wine-app.Domain/HttpResponseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace wine_app.Domain
+{
+    public static class HttpResponseHandler
+    {
+        public static async Task<string> GetError(HttpResponseMessage response)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(responseString);
+
+            var errorMessage = string.Empty;
+
+            foreach(var error in errors)
+            {
+                var errorString = string.Empty;
+
+                foreach (var err in error.Value)
+                {
+                    errorString = string.Join(".", err);
+                }
+
+                errorMessage = string.IsNullOrEmpty(errorMessage) ? errorString : errorMessage + "," + errorString;
+            }
+
+            return errorMessage;
+        }
+    }
+}
---
+++ wine-app.Domain/Result.cs
@@ -0,0 +1,40 @@
+﻿namespace wine_app.Domain
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Error { get; set; }
+
+        public T Data { get; set; }
+
+        public Result() { }
+
+        public Result(bool isSuccess, string errors, T data)
+        {
+            IsSuccess = isSuccess;
+            Error = errors;
+            Data = data;
+        }
+    }
+
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Error { get; set; }
+
+        public Result() { }
+
+        public Result(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+
+        public Result(string errors, bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+            Error = errors;
+        }
+    }
+}
