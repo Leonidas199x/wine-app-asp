b23156ac1af6a0106e517c2f1af9dd5d44a99d44
Leonidas199x <toby.w.prince@gmail.com>
2021-04-01 18:19:01 +0100

    Made ID nullable

--- wine-app/Models/Country/EditableCountryViewModel.cs
+++ wine-app/Models/Country/EditableCountryViewModel.cs
@@ -4,11 +4,9 @@ namespace wine_app.Models.Country
 {
     public class EditableCountryViewModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Display(Name="Country"), Required]
-        [ScaffoldColumn(true)]
-        [StringLength(50, ErrorMessage = "The {0} value cannot exceed {1} characters.")]
         public string Name { get; set; }
 
         public string Note { get; set; }
