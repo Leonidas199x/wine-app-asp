37ee4d8641d73c979a3900edb794855de358f5d0
Leonidas199x <toby.w.prince@gmail.com>
2021-04-09 21:17:02 +0100

    Addition of new Grape controller methods

--- wine-app/Controllers/GrapeController.cs
+++ wine-app/Controllers/GrapeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using wine_app.Domain;
 using wine_app.Domain.Grape;
 using wine_app.Models.Grape;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace wine_app.Controllers
 {
@@ -20,6 +22,81 @@ namespace wine_app.Controllers
             _grapeMapper = grapeMapper;
         }
 
+        #region grape
+        [HttpGet]
+        public async Task<IActionResult> ListGrapes()
+        {
+            var domainGrapes = await _grapeService.GetGrapes().ConfigureAwait(false);
+            var grapeViewModel = _grapeMapper.Map<IEnumerable<GrapeViewModel>>(domainGrapes.Data);
+            var grapeColoursResult = await _grapeService.GetAllColours().ConfigureAwait(false);
+
+            foreach(var grape in grapeViewModel)
+            {
+                grape.GrapeColourString = grapeColoursResult.Data.Where(x => x.Id == grape.GrapeColourId).FirstOrDefault().Colour;
+            }
+
+            var viewModel = new Result<IEnumerable<GrapeViewModel>>(grapeColoursResult.IsSuccess, grapeColoursResult.Error, grapeViewModel);
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> InsertGrape()
+        {
+            var viewModel = new EditableGrapeViewModel();
+            var grapeColours = await _grapeService.GetAllColours().ConfigureAwait(false);
+
+            var selectList = new List<SelectListItem>()
+            {
+                new SelectListItem(),
+            };
+
+            foreach(var colour in grapeColours.Data)
+            {
+                var listItem = new SelectListItem
+                {
+                    Value = colour.Id.ToString(),
+                    Text = colour.Colour,
+                };
+
+                selectList.Add(listItem);
+            }
+
+            viewModel.GrapeColours = selectList;
+
+            return View(new Result<EditableGrapeViewModel>(viewModel));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditGrape(int Id, bool isSuccess = false)
+        {
+            var domainGrape = await _grapeService.GetGrape(Id).ConfigureAwait(false);
+            var viewModel = _grapeMapper.Map<EditableGrapeViewModel>(domainGrape.Data);
+            var grapeColours = await _grapeService.GetAllColours().ConfigureAwait(false);
+
+            var selectList = new List<SelectListItem>()
+            {
+                new SelectListItem(),
+            };
+
+            foreach (var colour in grapeColours.Data)
+            {
+                var listItem = new SelectListItem
+                {
+                    Value = colour.Id.ToString(),
+                    Text = colour.Colour,
+                    Selected = colour.Id == viewModel.Id,
+                };
+
+                selectList.Add(listItem);
+            }
+
+            viewModel.GrapeColours = selectList;
+
+            return View(new Result<EditableGrapeViewModel>(viewModel, isSuccess));
+        }
+        #endregion
+
+        #region colours
         [HttpGet]
         public async Task<IActionResult> ListColours()
         {
@@ -54,7 +131,9 @@ namespace wine_app.Controllers
                 return RedirectToAction("EditColour", "Grape", new { id = model.Data.Id, IsSuccess = true });
             }
 
-            return View();
+            var viewModel = new Result<EditableGrapeColourViewModel>(saveResult.IsSuccess, saveResult.Error, model.Data);
+
+            return View(viewModel);
         }
 
         [HttpGet]
@@ -105,5 +184,6 @@ namespace wine_app.Controllers
                     return BadRequest();
             }
         }
+        #endregion
     }
 }
