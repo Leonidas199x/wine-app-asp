bb0199541da3f92a4f41a3c20e25cd66c8455482
Leonidas199x <toby.w.prince@gmail.com>
2021-04-09 21:15:34 +0100

    Addition of Grape models

---
+++ wine-app/Models/Grape/EditableGrapeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace wine_app.Models.Grape
+{
+    public class EditableGrapeViewModel
+    {
+        public int? Id { get; set; }
+
+        [Display(Name = "Name"), Required]
+        public string Name { get; set; }
+
+        public int GrapeColourId { get; set; }
+
+        [Display(Name = "Grape Colour"), Required]
+        public List<SelectListItem> GrapeColours { get; set; }
+
+        [Display(Name = "Note")]
+        public string Note { get; set; }
+    }
+}
---
+++ wine-app/Models/Grape/GrapeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace wine_app.Models.Grape
+{
+    public class GrapeViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int GrapeColourId { get; set; }
+
+        public string GrapeColourString { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
