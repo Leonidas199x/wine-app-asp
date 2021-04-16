e0474326f5ab804efaba023a4dee880a87c0e2ac
Leonidas199x <toby.w.prince@gmail.com>
2021-04-16 17:37:12 +0100

    Moved mapbox js code into own file and referenced in view

---
+++ wine-app/wwwroot/js/map-box.js
@@ -0,0 +1,35 @@
+﻿function renderMap(accessToken, longitude, latitude, zoom, isoCode) {
+    mapboxgl.accessToken = accessToken;
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [longitude, latitude],
+        zoom: zoom
+    });
+
+    map.on('load', function () {
+    map.addLayer(
+        {
+            id: 'country-boundaries',
+            source: {
+                type: 'vector',
+                url: 'mapbox://mapbox.country-boundaries-v1',
+            },
+            'source-layer': 'country_boundaries',
+            type: 'fill',
+            paint: {
+                'fill-color': '#d2361e',
+                'fill-opacity': 0.4,
+            },
+        },
+        'country-label'
+    );
+
+    map.setFilter('country-boundaries', [
+        "in",
+        "iso_3166_1",
+        isoCode
+        ]);
+    });
+};
