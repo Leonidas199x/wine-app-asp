08a7b06bfe2ae212fb6461f153f3f71211cbf859
Leonidas199x <toby.w.prince@gmail.com>
2021-04-27 21:12:03 +0100

    Addition of paging logic

--- wine-app/Models/PagedList.cs
+++ wine-app/Models/PagedList.cs
@@ -9,5 +9,48 @@
         public int TotalPages { get; set; }
 
         public T Data { get; set; }
+
+        public int PreviousPage => Page - 1;
+
+        public int NextPage => Page + 1;
+
+        public int StartPage => GetStartPage(Page, TotalPages);
+
+        public int EndPage => GetEndPage(Page, TotalPages);
+
+        private int GetStartPage(int page, int totalPages)
+        {
+            if((page - 3) < 1)
+            {
+                return 1;
+            }
+
+            if(page > totalPages - 7)
+            {
+                return TotalPages - 7;
+            }
+
+            return Page - 3;
+        }
+
+        private int GetEndPage(int page, int totalPages)
+        {
+            if(page <= 4)
+            {
+                return 7;
+            }
+
+            if ((page + 3) > totalPages)
+            {
+                return totalPages;
+            }
+
+            if(page > totalPages - 7)
+            {
+                return totalPages;
+            }
+
+            return Page + 3;
+        }
     }
 }
