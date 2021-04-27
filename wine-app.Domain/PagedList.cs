37f2c9c5e9efa0816e5b69de5e6501d826585a2b
Leonidas199x <toby.w.prince@gmail.com>
2021-04-27 20:13:53 +0100

    Addition of PagedList

---
+++ wine-app.Domain/PagedList.cs
@@ -0,0 +1,13 @@
+﻿namespace wine_app.Domain
+{
+    public class PagedList<T>
+    {
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public T Data { get; set; }
+    }
+}
