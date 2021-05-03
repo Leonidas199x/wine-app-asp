46e40f3639a0c597919af61b137d4d428ba7de16
Leonidas199x <toby.w.prince@gmail.com>
2021-05-03 19:33:20 +0100

    Simplification of delete javascript

---
+++ wine-app/wwwroot/js/delete.js
@@ -0,0 +1,16 @@
+﻿function deleteEntity(controller, id, name, singular, plural) {
+    if (confirm(`Are you sure you want to delete '${name}' from the list of available ${plural}?`)) {
+        $.ajax({
+            url: `/${controller}/delete/${id}`,
+            type: 'DELETE',
+            error: function (err) {
+                console.log(err);
+                alert(`Something went wrong, cannot delete ${singular} '${name}' and ID ${id}. Error code: ${err.status}`);
+            },
+            success: function () {
+                window.location.href = `/${controller}/list`;
+            },
+            async: true,
+        });
+    }
+}
