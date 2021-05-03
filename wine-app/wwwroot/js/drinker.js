060a3dfa15579d4c0fb97a877199c49fea21282f
Leonidas199x <toby.w.prince@gmail.com>
2021-05-03 14:10:32 +0100

    Addition of drinker list view, and js file

---
+++ wine-app/wwwroot/js/drinker.js
@@ -0,0 +1,16 @@
+﻿function deleteDrinker(id, name) {
+    if (confirm(`Are you sure you want to delete '${name}' from the list of available drinkers?`)) {
+        $.ajax({
+            url: `/Drinker/Delete/${id}`,
+            type: 'DELETE',
+            error: function (err) {
+                console.log(err);
+                alert(`Something went wrong, cannot delete drinker with name '${name}' and ID ${id}. Error code: ${err.status}`);
+            },
+            success: function () {
+                window.location.href = `/Drinker/List`;
+            },
+            async: true,
+        });
+    }
+}
